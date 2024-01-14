e66e550e7193419b23f6f2141249714c7fddd079
Predrag <predrag.tosic@elfak.rs>
2024-01-14 15:09:57 +0100

    Added Repository project pattern

---
+++ TwitterApi/DAL/Repository/IRepository/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repository.IRepository
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<T> GetOne(int id);
+        Task<IQueryable<T>> GetAll();
+        IQueryable<T> Find(Expression<Func<T, bool>> predicate);
+        Task Add(T obj);
+        void Delete(T obj);
+        void Update(T obj);
+    }
+}
