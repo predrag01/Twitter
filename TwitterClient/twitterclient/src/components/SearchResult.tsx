8cdcf336e6a4f5cb912245a5f49e511af5dceaaa
Predrag <predrag.tosic@elfak.rs>
2024-01-14 22:27:12 +0100

    Updated search bar

--- TwitterClient/twitterclient/src/components/SearchResult.tsx
+++ TwitterClient/twitterclient/src/components/SearchResult.tsx
@@ -1,8 +1,12 @@
 import { User } from "../models/user.model";
+import myimage from "./../assets/noProfilePicture.png"
 
 const SearchResult = (props: {result: User}) => {
   return (
-    <div>{props.result.username}</div>
+    <div className="searchResult">
+      <img className="searchResultImg" src={myimage} alt={props.result.username}/>
+      <label className="searchResultTitle">{props.result.username}</label>
+    </div>
   );
 };
 
